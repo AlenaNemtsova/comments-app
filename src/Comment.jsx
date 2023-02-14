fa6cb0f19eb664407131386d6a103334ad5913f5
Alena Nemtsova <jelenatbs@gmail.com>
2023-02-14 18:52:55 +0300

     fix: spread instead unshift

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -10,14 +10,13 @@ export default function Comment() {
     const handleSubmit = (event) => {
         event.preventDefault();
         if (!newItem) return;
-        console.log('submitted');
 
-        addItem(newItem);
-        setNewItem('');
+        addItem(newItem); //вызываем функцию добавления коммента и передаём, как параметр, новый коммент из поля ввода
+        setNewItem(''); //очищает поле ввода после отправки сообщения
     }
 
     const addItem = (comment) => {
-        const id = items.length ? items.length + 1 : 1;
+        const id = items.length ? items.length + 1 : 1; //определяем id для нового коммента
 
         const getDate = new Date();
         const options = { //задаём формат вывода компонентов даты
@@ -27,22 +26,26 @@ export default function Comment() {
             hour: "numeric",
             minute: "numeric",
         }
-        const date = getDate.toLocaleString("ru", options);
+        const date = getDate.toLocaleString("ru", options); //определяем дату нового коммента
 
-        const addedItem = { id: id, text: comment, date: date };
-        items.unshift(addedItem);
-        localStorage.setItem('comments', JSON.stringify(items)); //для localStorage: ключ=comments, значение=массив с объектами 
+        const addedItem = { id: id, text: comment, date: date }; //сохраняем параметры добавленного коммента для хранилища
+        // items.unshift(addedItem); //нельзя мутировать массив напрямую, 
+        //поэтому создаём копию массива с комментариями:
+        const newArr = [addedItem, ...items] //первый элемент – новый коммент, дальше все старые комменты 
+        setItems(newArr); //в items кладём массив с новым комментом и сохраняем его в localStorage:
+        localStorage.setItem('comments', JSON.stringify(newArr)); //для localStorage: ключ=comments, значение=массив с объектами 
     }
 
     const handleChange = (event) => {
-        setNewItem(event.target.value.trim())
+        setNewItem(event.target.value) //отлавливаем изменения в поле при вводе с клавиатуры, меняем newItem
     }
 
+    //при первом рендере страницы достаём сохранённые комменты из localStorage
     useEffect(() => {
-        let itemsFromStorage = JSON.parse(localStorage.getItem('comments')); //проверяем, есть ли в localStorage сохраненные значения
+        let itemsFromStorage = JSON.parse(localStorage.getItem('comments')); //обрабатываем сохранённое
         console.log(itemsFromStorage);
-        if (itemsFromStorage)
-            setItems(itemsFromStorage)
+        if (itemsFromStorage) //проверяем, есть ли вообще в localStorage сохранённые значения
+            setItems(itemsFromStorage) //в items (массив с комментами), кладём все значения из localStorage, чтобы затем отрисовать items.map()
     }, [])
 
     console.log('render');
@@ -55,7 +58,8 @@ export default function Comment() {
             </div>
             <form className='form' onSubmit={handleSubmit}>
                 <label className='input-label'>Add your comment
-                    <textarea onChange={handleChange} type="text" className="input" rows="10"></textarea>
+                    <textarea onChange={handleChange} value={newItem} type="text" className="input" rows="10"></textarea>
+                    {/* у textarea не забыть указать value!!!, которое будет очищаться setNewItem('');  */}
                 </label>
                 <button type="submit" className="send-btn">Send</button>
             </form>
